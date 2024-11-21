d75b06fda0b83c5c16851f36eda3fccfee9df117
emzhran <zachran750@gmail.com>
2024-11-21 22:52:20 +0700

    menambahkan properti nim, alamat, notelpon pada data class DataSiswa

--- app/src/main/java/com/example/mvvm118/model/DataSiswa.kt
+++ app/src/main/java/com/example/mvvm118/model/DataSiswa.kt
@@ -3,5 +3,8 @@ package com.example.mvvm118.model
 data class DataSiswa(
     val nama : String="",
     val gender : String="",
-    val email : String=""
+    val email : String="",
+    val nim : String="",
+    val alamat : String="",
+    val notelpon : String=""
 )
